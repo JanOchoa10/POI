64134a37eeb4dfae1aac9bd6f4db077204571088
Jan Ochoa <jan8a00@gmail.com>
2022-11-09 17:53:47 -0600

    No favicon
    Prueba

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,15 +5,15 @@ import Chat from "../components/Chat";
 const Home = () => {
     // Asignamos icono en la pestaña
     //Creamos la función para asignar el icono
-    function setFavicons(favImg) {
-        let headTitle = document.querySelector('head');
-        let setFavicon = document.createElement('link');
-        setFavicon.setAttribute('rel', 'shortcut icon');
-        setFavicon.setAttribute('href', favImg);
-        headTitle.appendChild(setFavicon);
-    }
-    //Asignamos el icono de nuestro sitio web
-    setFavicons('https://i.ibb.co/rkKS779/logo-Robbin128.jpg');
+    // function setFavicons(favImg) {
+    //     let headTitle = document.querySelector('head');
+    //     let setFavicon = document.createElement('link');
+    //     setFavicon.setAttribute('rel', 'shortcut icon');
+    //     setFavicon.setAttribute('href', favImg);
+    //     headTitle.appendChild(setFavicon);
+    // }
+    // //Asignamos el icono de nuestro sitio web
+    // setFavicons('https://i.ibb.co/rkKS779/logo-Robbin128.jpg');
 
 
     return (
